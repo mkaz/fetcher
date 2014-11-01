4d355219306d60f717756f6eabb05a343f1d719e
Marcus Kazmierczak <marcus@mkaz.com>
2014-11-01 14:59:25 -0700

    Switch fetcher_test to use net/http/httptest for simluarting http server responses

--- fetcher_test.go
+++ fetcher_test.go
@@ -1,48 +1,47 @@
 package fetcher_test
 
 // Tests for Fetcher
-//
-// Testing requires a web server to response to tests requests.
-// If running Apache locally with PHP, create the followiing as
-// echo.php in the localhost directory:
-//
-//      <?php
-//
-// 	    if ( isset( $_REQUEST['p'] ) ) {
-//   		$p = strip_tags( $_REQUEST['p'] );
-// 	    }
-//
-// 	    if ( empty( $p ) ) {
-// 		   echo "No P";
-// 	    } else {
-// 		   echo $p;
-// 	    }
+// Users net/http/httptest for server stubbing
 
 import (
-    "github.com/mkaz/fetcher"
+
+    "fmt"
+    "net/http"
+    "net/http/httptest"
     "testing"
+
+    "github.com/mkaz/fetcher"
 )
 
 // This tests basic GET request
 func TestGetBasic(t *testing.T) {
-    url := "http://localhost/echo.php"
+    ts := httptest.NewServer(http.HandlerFunc(func( w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "hola mundo")
+    }))
+    defer ts.Close()
+
     f := fetcher.NewFetcher()
-    result, err := f.Fetch(url, "GET")
+    result, err := f.Fetch(ts.URL, "GET")
     if err != nil {
         t.Errorf("Error: %v", err)
     }
 
-    if result != "No P" {
+    if result != "hola mundo" {
         t.Errorf("Unexpected result: %v", result)
     }
 }
 
 // This tests GET request with passing in a parameter
 func TestGetParams(t *testing.T) {
-    url := "http://localhost/echo.php"
+    ts := httptest.NewServer(http.HandlerFunc(func( w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, r.FormValue("p"))
+    }))
+    defer ts.Close()
+
+
     f := fetcher.NewFetcher()
     f.Params.Add("p", "hello")
-    result, err := f.Fetch(url, "GET")
+    result, err := f.Fetch(ts.URL, "GET")
     if err != nil {
         t.Errorf("Error: %v", err)
     }
